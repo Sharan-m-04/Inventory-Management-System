a2a9adc93e46dfc87d1b5aa5912e44e960f8713f
Sharan M <msharan.hnp@gmail.com>
2024-02-06 23:08:24 +0530

    added nav, about and footer

--- Inventory_Management_System/Inventory_Management_System/settings.py
+++ Inventory_Management_System/Inventory_Management_System/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ Inventory_Management_System/home/static/script.js
@@ -0,0 +1,9 @@
+//Navigation Burgur
+burger = document.querySelector('.burger');
+navig = document.querySelector('.navig');
+navul = document.querySelector('.navul');
+ 
+burger.addEventListener('click', ()=>{
+    navul.classList.toggle('v-class-resp');
+    navig.classList.toggle('h-nav-resp');
+});
