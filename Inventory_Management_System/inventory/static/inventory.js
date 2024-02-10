7972e455a415f33fc78e68ed22b7506b97c9a927
Sharan M <msharan.hnp@gmail.com>
2024-02-10 17:06:54 +0530

    designed inventory page and transactions page

--- Inventory_Management_System/inventory/static/inventory.js
+++ Inventory_Management_System/inventory/static/inventory.js
@@ -7,16 +7,3 @@ burger.addEventListener('click', () => {
     navul.classList.toggle('v-class-resp');
     navig.classList.toggle('h-nav-resp');
 });
-
-// Dashboard section
-function logout() {
-    window.open("/accounts/logout/", "_self");
-}
-
-function inventoryRedir() {
-    window.open("/inventory", "_self");
-}
-
-function transactionsRedir() {
-    window.open("/transactions", "_self");
-}
--- Inventory_Management_System/transactions/views.py
+++ Inventory_Management_System/transactions/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+import datetime
+
 
 # Create your views here.
 def transactions(request):
-    return render(request, 'transactions.html')
+    dateTimeNow = datetime.datetime.now()
+    dateTimeFormat = dateTimeNow.strftime("%Y-%m-%d, %H:%M")
+    return render(request, 'transactions.html', {
+        'dateTimeFormat': dateTimeFormat,
+    })
