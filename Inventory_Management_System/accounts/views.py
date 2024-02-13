72745f8210198fc030a1c40aaa30fb769069c907
Sharan M <101189789+Sharan-m-04@users.noreply.github.com>
2024-02-13 16:26:47 +0530

    Update views.py

--- Inventory_Management_System/accounts/views.py
+++ Inventory_Management_System/accounts/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
-
 def signup(request):
     if request.method == 'POST':
         name = request.POST['username']
