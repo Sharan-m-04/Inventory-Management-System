eed90305b284e1b31e058226e8a90714c3c1b00e
Sharan M <msharan.hnp@gmail.com>
2024-02-18 23:45:06 +0530

    designed home page

--- Inventory_Management_System/home/views.py
+++ Inventory_Management_System/home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from inventory.models import Product
+from transactions.models import Transaction
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    low_quantity_products = Product.objects.filter(quantity__lt=10).order_by('quantity')
+    recent_transactions = Transaction.objects.order_by('-datetime')[:10]
+    return render(request, 'home.html', {'recent_transactions': recent_transactions, 'low_quantity_products': low_quantity_products})
