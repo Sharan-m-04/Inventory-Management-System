e3a51bc826ace8f2c083b69b6f741733a9bc5034
Sharan M <101189789+Sharan-m-04@users.noreply.github.com>
2024-02-21 08:13:00 +0530

    Update views.py

--- Inventory_Management_System/feedbacks/views.py
+++ Inventory_Management_System/feedbacks/views.py
@@ -5,7 +5,6 @@ from .models import Feedback
 def submit_feedback(request):
     feedbacks = Feedback.objects.all()
     is_superuser = request.user.is_superuser
-
     return render(request, 'feedbacks.html', {
         'feedbacks': feedbacks,
         'is_superuser':is_superuser,
